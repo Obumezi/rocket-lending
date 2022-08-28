64ab6cdb7dd929cf298ee11c26d07cca347e4ce1
obumezi <eziamakaobum@yahoo.co.uk>
2022-08-28 16:22:30 +0100

    created trasnfer api

--- transfers.js
+++ transfers.js
@@ -1,11 +1,31 @@
-const form = document.getElementById("myModal")
+var myHeaders = new Headers();
 
-form.addEventListener(onclick, makeTransfer)
+fetch("https://fsi.ng/api/v1/flutterwave/v3/transfers", {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json",
+        "sandbox-key": "EJHSstlTSs5ZqXh3s2jVbcttZMANxSVq1661689640",
+        "Authorization": "dskjds",
+    },
 
+    body: JSON.stringify({
+        "account_bank": "044",
+        "account_number": "0690000040",
+        "amount": 5500,
+        "narration": "Akhlm Pstmn Trnsfr xx007",
+        "currency": "NGN",
+        "reference": "akhlm-pstmnpyt-rfxx007_PMCKDU_1",
 
-function makeTransfer(e) {
-    e.preventDefault()
+        "debit_currency": "NGN"
+    })
 
-  
-    
-}
+}).then(res => {
+    return res.json()
+})
+    .then(data => 
+        {
+            let successMessage = data.message;
+            console.log(successMessage)
+            console.log(data);
+        })
+    .catch(error => console.log("error"))
