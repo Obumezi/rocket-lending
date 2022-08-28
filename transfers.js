cabaf09efc644dacc4ade6daa52fa8d7619a1fec
obumezi <eziamakaobum@yahoo.co.uk>
2022-08-28 20:15:10 +0100

    created trasnfer api fix

--- transfers.js
+++ transfers.js
@@ -1,31 +1,57 @@
 var myHeaders = new Headers();
 
-fetch("https://fsi.ng/api/v1/flutterwave/v3/transfers", {
-    method: 'POST',
-    headers: {
-        "Content-type": "application/json",
-        "sandbox-key": "EJHSstlTSs5ZqXh3s2jVbcttZMANxSVq1661689640",
-        "Authorization": "dskjds",
-    },
+const bankName = document.getElementById("bankName").value
+const accountNumber = document.getElementById("accountNumber").value
+const amount = document.getElementById("amount").value
 
-    body: JSON.stringify({
-        "account_bank": "044",
-        "account_number": "0690000040",
-        "amount": 5500,
-        "narration": "Akhlm Pstmn Trnsfr xx007",
-        "currency": "NGN",
-        "reference": "akhlm-pstmnpyt-rfxx007_PMCKDU_1",
+const form = document.getElementById("myModal")
 
-        "debit_currency": "NGN"
+
+
+
+function transferNow(e) {
+    e.preventDefault();
+
+    fetch("https://fsi.ng/api/v1/flutterwave/v3/transfers", {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            "sandbox-key": "EJHSstlTSs5ZqXh3s2jVbcttZMANxSVq1661689640",
+            "Authorization": "dskjds",
+        },
+
+        body: JSON.stringify({
+            "account_bank": "044",
+            "account_number": accountNumber,
+            "amount": amount,
+            "narration": "Akhlm Pstmn Trnsfr xx007",
+            "currency": "NGN",
+            "reference": "akhlm-pstmnpyt-rfxx007_PMCKDU_1",
+
+            "debit_currency": "NGN",
+
+        })
+
+    }).then(res => {
+        return res.json()
     })
-
-}).then(res => {
-    return res.json()
-})
-    .then(data => 
-        {
+        .then(data => {
             let successMessage = data.message;
             console.log(successMessage)
             console.log(data);
         })
-    .catch(error => console.log("error"))
+        .catch(error => console.log('not working', error))
+
+    if (accountNumber === "0236755798" && amount === "50000") {
+        console.log("gooo to go")
+
+    } else (console.log("check again"))
+
+
+}
+
+form.addEventListener("submit", transferNow)
+
+
+
+
